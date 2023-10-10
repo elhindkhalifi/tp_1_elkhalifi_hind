0a0091a9a3929c8cb82a752b11aafb05efe38386
elhindkhalifi <elhindkhalifi@gmail.com>
2023-10-10 14:47:40 -0400

    init

--- function.php
+++ function.php
@@ -6,12 +6,12 @@ function passwordLengthIsValid($passwordToValid)
     $length= strlen($passwordToValid);
     $responses=[
         "isValid"=>true,
-        "msg"=>"<h2><center>Mot de passe correct!</center></H2>"
+        "msg"=>"<h2><center>Mot de passe valide!</center></H2>"
     ];
     if ($length < 6 ||$length  > 10) {
         $responses=[
         'isValid'=>false,
-        "msg"=>"<h2><center>Mot de passe incorrect!</H2></center></BR>Erreur : Le mot de passe doit avoir entre 6 et 10 caracteres."
+        "msg"=>"<h2><center>Mot de passe non valide!</H2></center></BR>Erreur : Le mot de passe doit avoir entre 6 et 10 caracteres."
         ];
       }  
     return $responses; 
